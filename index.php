966f7a0c87feaaf69b92bb374d96058587f54979
EricCossio <cossiovargase@gmail.com>
2025-10-14 19:40:03 -0600

    first commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+// index.php
+?>
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title>Index - Gamer</title>
+</head>
+
+<body>
+    <h1><?php echo 'Aqui Gamer reportandome'; ?></h1>
+</body>
+
+</html>
